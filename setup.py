40fe05f765f322411023ed1d659684466164a098
Christian Kellner <gicmo@gnome.org>
2011-09-03 10:20:57 +0200

    Refine setup.py for distribution in the PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-"""Neuroshare Bindings for Python
+"""Python Bindings for Neuroshare
+---------------------------------
 
+The Neuroshare API is an standardize API for accessing
+neurophysiology data stored in vendor-specific binary formats
+in a vendor-neutral way. See
+                   http://neuroshare.org
+for more information.
+
+The aim of this library is to provide a high level interface
+to the Neuroshare API, i.e. it focuses on API usability more
+then being a mere python version of the C API. Thus none of
+the original Neuroshare API calls are directly exposed but the
+interface is through python objections that resemble (more or
+less) the Neuroshare Entities.
 """
 
 from distutils.core import setup, Extension
@@ -13,16 +26,21 @@ classifiers = [
         'Programming Language :: Python',
         'Programming Language :: C',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Intended Audience :: Developers']
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules']
 
 native_ext = Extension('neuroshare._capi',
                        include_dirs = [np.get_include()],
                        sources = ['capi/nspy_glue.c'])
 
 setup (name             = 'neuroshare',
-       version          = '0.8',
+       version          = '0.8.2',
        author           = 'Christian Kellner',
        author_email     = 'kellner@biologie.uni-muenchen.de',
+       url              = 'https://github.com/G-Node/python-neuroshare',
+       keywords         = ['neuroshare'],
        description      = __doc__.split("\n")[0],
        long_description = "\n".join(__doc__.split("\n")[2:]),
        classifiers      = classifiers,
