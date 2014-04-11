0174957325bcd2b92c3bd17be05f01cb1e46321a
Christian Kellner <christian@kellner.me>
2014-04-11 17:10:17 +0200

    [doc] remove mathjax plugin (unused)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ import sys, os, pkg_resources
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
